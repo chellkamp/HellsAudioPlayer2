1e7c1cf3508c918128acc666c7971cd0d32b78a7
unknown <chellkamp@yahoo.com>
2022-09-24 01:26:44 -0400

    Introduction of util module and wait splash.

--- app/src/main/java/com/hellscode/hellsaudioplayer/MainActivity.kt
+++ app/src/main/java/com/hellscode/hellsaudioplayer/MainActivity.kt
@@ -2,10 +2,30 @@ package com.hellscode.hellsaudioplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hellscode.util.WaitSplash
 
 class MainActivity : AppCompatActivity() {
+    private var w: WaitSplash? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        w = WaitSplash(this, R.drawable.ic_wait)
+
+        w!!.start()
+
+        val t = Thread(
+            {
+                Thread.sleep(5000)
+
+                runOnUiThread{w!!.stop()}
+            }
+        )
+        t.start()
+
     }
 }
